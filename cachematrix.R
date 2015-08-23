3c7eb09af9b8935517ef635c5b0d6578b685bfe7
jpradhan86 <jitendra.pradhan86@gmail.com>
2015-08-23 13:07:45 +0530

    commit the catch matrix for inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Create the matrix and Catch the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	I <- NULL
+        set <- function(y) {
+		x <<- y
+		I <<- NULL
+	}
+	get <- function() x
+	setInverse <- function(solve) I <<- solve
+	getInverse <- function() I
+									        	list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Return inverse of matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	I <- x$getInverse()
+	if(!is.null(I)) {
+		message("getting cached data")
+		return(I)
+	}
+	data <- x$get()
+	I <- solve(data, ...)								x$setInverse(I)
+	I
 }
